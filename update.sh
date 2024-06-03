d2dd62a3302c1002c45bb8934df85086dd05f92e
NIXY Project <106488921+nixy-pro@users.noreply.github.com>
2024-06-03 15:35:21 +0700

    NIXY Project

--- update.sh
+++ update.sh
@@ -27,7 +27,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/nixy-pro/Atp/main/menu/menu.zip
+    wget https://raw.githubusercontent.com/nixy-pro/Atp/master/menu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
